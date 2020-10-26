d23c5ad4be5fb724b908195961b354ad81951458
tuip123 <tuip123xuzhenghao@outlook.com>
2020-10-26 14:57:47 +0800

    取消绑定使用Dialog

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -2,15 +2,36 @@
 const app = getApp()
 Page({
   data: {
-
+    dialogShow: false,
+    buttons: [{
+      text: '返回'
+    }, {
+      text: '解除绑定'
+    }],
   },
-  removeBinding:function(){
+  removeBinding: function () {
     console.log(app.globalData._openid)
-  }
-  ,
-  test:function(){
+    this.setData({
+      dialogShow: true
+    })
+  },
+  test: function () {
     wx.navigateTo({
       url: '../../pages/sendMsg/sendMsg',
     })
-  }
+  },
+  tapDialogButton(e) {
+    if(e.detail.index === 0){
+      console.log('返回')
+      this.setData({
+        dialogShow: false,
+      })
+    }
+    else if(e.detail.index === 1){
+      console.log('解除绑定')
+      this.setData({
+        dialogShow: false,
+      })
+    }
+  },
 })
