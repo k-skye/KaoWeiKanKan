9e2631b61a95cc0fa680101d3eeee5af1048c231
tuip123 <tuip123xuzhenghao@outlook.com>
2020-10-26 17:41:38 +0800

    新增提交用户建议界面

--- miniprogram/pages/sendMsg/sendMsg.js
+++ miniprogram/pages/sendMsg/sendMsg.js
@@ -1,11 +1,23 @@
 // pages/sendMsg/sendMsg.js
 Page({
   data: {
-
+    formData: {},
+    success: ''
   },
-  back:function(){
+  back: function () {
+    this.setData({
+      success: '已经收到您的反馈'
+    })
     wx.navigateBack({
       delta: 0,
     })
-  }
+  },
+  formInputChange(e) {
+    const {
+      field
+    } = e.currentTarget.dataset
+    this.setData({
+      [`formData.${field}`]: e.detail.value
+    })
+  },
 })
