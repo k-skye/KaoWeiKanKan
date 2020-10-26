d7d17d1c2a86ac6a780cb1d7daaf15257bbf0f0d
tuip123 <tuip123xuzhenghao@outlook.com>
2020-10-26 19:19:55 +0800

    优化反馈

--- miniprogram/pages/sendMsg/sendMsg.js
+++ miniprogram/pages/sendMsg/sendMsg.js
@@ -8,6 +8,7 @@ Page({
     this.setData({
       success: '已经收到您的反馈'
     })
+    sleep(1000)
     wx.navigateBack({
       delta: 0,
     })
@@ -21,3 +22,12 @@ Page({
     })
   },
 })
+function sleep(numberMillis) { 
+  var now = new Date(); 
+  var exitTime = now.getTime() + numberMillis; 
+  while (true) { 
+  now = new Date(); 
+  if (now.getTime() > exitTime) 
+  return; 
+  } 
+  }
